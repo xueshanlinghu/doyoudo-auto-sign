d41ff8324f5335a27430781f0b59c7d8645b9c5b
xueshanlinghu <wangzhuang88888888@126.com>
2020-10-26 11:06:24 +0800

    修改支持日志文件发送

--- doyoudo_auto_sign.py
+++ doyoudo_auto_sign.py
@@ -14,6 +14,8 @@ import requests
 import sys
 import random
 import json
+import logging
+import datetime
 
 # 登录url
 login_url = "https://www.doyoudo.com/api/user/login/"
@@ -41,7 +43,7 @@ say_contents = [
     "学如逆水行舟"
 ]
 
-def login(username, password):
+def login():
     """登录程序"""
     json_body = {
         "username": username,
@@ -51,23 +53,23 @@ def login(username, password):
     if res.status_code == 200:
         res.encoding = "utf-8"
         content = res.json()
-        # print(content)
+        # log_print(content)
         if content.get("errno") == 0:
-            print("登录成功！")
+            log_print("登录成功！")
             # 取必要信息
             info = {"token": content.get("results").get("token")}
-            # print(info)
+            # log_print(info)
             return info
         else:
             return None
 
 def cal_dyd_token(res):
     """计算dyd-token的值"""
-    print("正在计算加密值...")
+    log_print("正在计算加密值...")
     # 获取token
     token = res.get("token", None)
     if token is None:
-        print("token获取失败！程序结束！")
+        log_print("token获取失败！程序结束！")
         return None, None
     else:
         json_body = {
@@ -77,83 +79,83 @@ def cal_dyd_token(res):
         res = requests.post(cal_dyd_token_url, json=json_body, headers=headers)
         if res.status_code == 200:
             result = res.content.decode()
-            print(token, result)
+            log_print(token, result)
             return token, result
         else:
-            print("dyd-token的值计算不返回200有误")
+            log_print("dyd-token的值计算不返回200有误")
             return token, None
 
-def get_after_login_headers(token):
+def get_after_login_headers():
     """获取登录成功之后的headers"""
     # 使用copy方法，不会影响原程序集字典变量
     myheaders = headers.copy()
     myheaders['Token'] = token
     return myheaders
 
-def get_after_login_cookies(dyd_token):
+def get_after_login_cookies():
     """获取登录成功之后的cookies""" 
     cookies_dict = {"dyd-token": dyd_token}
     # 字典转换为可识别的cookiejar
     cookies = requests.utils.cookiejar_from_dict(cookies_dict)
     return cookies
 
-def auto_sign(token, dyd_token):
+def auto_sign():
     """自动签到"""
-    print("正在签到...")
-    myheaders = get_after_login_headers(token)
+    log_print("正在签到...")
+    myheaders = get_after_login_headers()
     # 签到不用传内容
     myheaders.pop('Content-Type')
-    cookies = get_after_login_cookies(dyd_token)
+    cookies = get_after_login_cookies()
     res = requests.post(sign_url, headers=myheaders, cookies=cookies)
     if res.status_code == 200:
         res.encoding = 'utf-8'
         content = res.json()
         if content.get("errno") == 0:
-            print("签到成功！")
+            log_print("签到成功！")
             results = content.get("results", "{}")
             constant = results.get("constant", "【获取不到】")
             reward = results.get("reward", "【获取不到】")
             score = results.get("score", "【获取不到】")
-            print(f"坚持天数：{constant}，本次签到奖励：{reward}，签到后雪糕数：{score}")
+            log_print(f"坚持天数：{constant}，本次签到奖励：{reward}，签到后雪糕数：{score}")
         else:
-            # print(content)
-            print(content.get("message", "获取不到信息！请联系作者检查！"))  
+            # log_print(content)
+            log_print(content.get("message", "获取不到信息！请联系作者检查！"))  
     else:
-        print(res.text)
-        print("签到点击失败！")
+        log_print(res.text)
+        log_print("签到点击失败！")
 
-def auto_say(token, dyd_token):
+def auto_say():
     """自动发言"""
-    print("正在说点什么...")
-    myheaders = get_after_login_headers(token)
-    cookies = get_after_login_cookies(dyd_token)
+    log_print("正在说点什么...")
+    myheaders = get_after_login_headers()
+    cookies = get_after_login_cookies()
     json_body = {
         "content": random.choice(say_contents),
         "image_urls": [],
         "at_userinfo_ids": [],
         "tag_title_list": []
     }
-    print(json_body)
+    log_print(json_body)
     res = requests.post(say_url, json=json_body, headers=myheaders, cookies=cookies)
     if res.status_code == 200:
         res.encoding = 'utf-8'
         content = res.json()
         if content.get("errno") == 0:
-            print("发言成功！")
+            log_print("发言成功！")
         else:
-            print(content)
-            print("发言失败！")
+            log_print(content)
+            log_print("发言失败！")
     else:
-        print(res.json())
-        print("发言点击失败！")
+        log_print(res.json())
+        log_print("发言点击失败！")
 
-def get_personal_info(token, dyd_token):
+def get_personal_info():
     """获取个人信息"""
-    print("正在获取个人信息...")
-    myheaders = get_after_login_headers(token)
+    log_print("正在获取个人信息...")
+    myheaders = get_after_login_headers()
     # 不用传内容
     myheaders.pop('Content-Type')
-    cookies = get_after_login_cookies(dyd_token)
+    cookies = get_after_login_cookies()
     res = requests.get(personal_info_url, headers=myheaders, cookies=cookies)
     if res.status_code == 200:
         res.encoding = 'utf-8'
@@ -171,17 +173,62 @@ def get_personal_info(token, dyd_token):
             }
             personal_info = json.dumps(personal_info, indent=4)
             # dumps之后，变为str，中文会变成unicode编码，需要先encode变为raw bytes，再decode回来显示
-            print(personal_info.encode().decode('unicode_escape'))
+            log_print(personal_info.encode().decode('unicode_escape'))
         else:
-            print(content)
-            print("获取个人信息失败！")
+            log_print(content)
+            log_print("获取个人信息失败！")
     else:
-        print(res.text)
-        print("访问获取个人信息失败！")
+        log_print(res.text)
+        log_print("访问获取个人信息失败！")
+
+# 在容器里运行时时间为UTC时间，不是北京时间，需要进行调整
+def beijing(sec, what):
+    beijing_time = datetime.datetime.now() + datetime.timedelta(hours=8)
+    return beijing_time.timetuple()
+
+def log_setting():
+    """配置日志设置"""
+    LOG_FILE_NAME = "log.log"
+    LOG_PATH = LOG_FILE_NAME
+    log_level = logging.INFO
+    # 在容器里运行时时间为UTC时间，不是北京时间，需要进行调整
+    logging.Formatter.converter = beijing
+    logging.basicConfig(level=log_level,
+                        format='[%(asctime)s] - [line:%(lineno)d] - %(levelname)s: %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename=LOG_PATH,
+                        filemode='a')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level)
+    return logger
+
+def log_print(msg, level="info", to_log_file=True, to_console=True):
+    """
+    日志输出封装功能
+    :param msg: 要输出的信息
+    :param level: 日志级别，一般有debug, info, warning, error, critical等
+    :param to_log_file: 是否保存到日志文件中
+    :param to_console: 是否在控制台输出
+    """
+    if to_log_file:
+        if level == 'debug':
+            logger.debug(msg)
+        elif level == 'info':
+            logger.info(msg)
+        elif level == 'warning':
+            logger.warning(msg)
+        elif level == 'error':
+            logger.error(msg)
+        elif level == 'critical':
+            logger.critical(msg)
+    if to_console:
+        print(msg)
 
 
-def main():
-    """主入口函数"""
+if __name__ == '__main__':
+    # 日志配置
+    logger = log_setting()
+
     if len(sys.argv) != 3:
         raise Exception("传入参数不正确，第一个传入参数为登入的账号，第二个传入账户为密码")
 
@@ -189,21 +236,19 @@ def main():
     password = sys.argv[2]
 
     # 登录
-    res = login(username, password)
+    res = login()
     if res:
         token, dyd_token = cal_dyd_token(res)
-        if dyd_token is None: print("计算dyd-token的值失败"); return
-        # 检测并自动签到
-        auto_sign(token, dyd_token)
-        # 发言
-        auto_say(token, dyd_token)
-        # 获取个人信息
-        get_personal_info(token, dyd_token)
+        if dyd_token is None:
+            log_print("计算dyd-token的值失败，程序结束")
+        else:
+            # 检测并自动签到
+            auto_sign()
+            # 发言
+            auto_say()
+            # 获取个人信息
+            get_personal_info()
     else:
-        print("登录失败！")
+        log_print("登录失败！")
 
-    print("运行完毕！感谢您的使用！")
-
-
-if __name__ == '__main__':
-    main()
+    log_print("运行完毕！感谢您的使用！")
